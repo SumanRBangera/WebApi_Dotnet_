f3bdd6e84ea2ef13cbfb00f4becc6b4cbf799703
SumanBangera <sumanrbangera@gmail.com>
2022-10-17 20:10:11 +0530

    Add project files.

---
+++ WebApi_Dotnet_/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi_Dotnet_.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+//Configure the Sql Server Database ConnectionStrings
+builder.Services.AddDbContext<API_AngularContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("TEConnection")));
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(c => {
+    c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
